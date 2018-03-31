a0d9b47c19cbae6723a19da2b4b0d6d9cc65497b
Ismail Kutlu <ismail.kutlu@student.arteveldehs.be>
2018-03-31 11:49:01 +0200

    added screens and components map

---
+++ app/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <nav>
+                Gentlestudent footer
+            </nav>
+        );
+    }
+}
+  
+export default Footer;
---
+++ app/src/Components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav>
+                Gentlestudent nav
+            </nav>
+        );
+    }
+}
+  
+export default Nav;
