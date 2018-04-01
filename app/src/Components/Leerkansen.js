851c68c0c0c8d6eb738b7347f7430df4ca464e12
Ismail Kutlu <ismail.kutlu@student.arteveldehs.be>
2018-04-01 18:37:22 +0200

    updated startpage eyecather & leerkansen

---
+++ app/src/Components/Leerkansen.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Leerkansen extends Component {
+    render() {
+        return (
+            <div id="leerkansen">
+                <div className="container">
+                    <div className="content">
+                        <h1>Leerkansen</h1>
+                        
+                        <div class="card-container">
+                            <div class="card-item">1</div>
+                            <div class="card-item">2</div>
+                            <div class="card-item">3</div>  
+                            <div class="card-item">4</div>
+                            <div class="card-item">5</div>
+                            <div class="card-item">6</div>  
+                            <div class="card-item">7</div>
+                            <div class="card-item">8</div>
+                            <div class="card-item">9</div>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Leerkansen;
--- app/src/Screens/StartPage.js
+++ app/src/Screens/StartPage.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import Nav from '../Components/Nav';
-import Footer from '../Components/Footer';
+import Nav from './../Components/Nav';
+import Leerkansen from './../Components/Leerkansen';
+import Footer from './../Components/Footer';
+
+import eyecather from './../assets/eyecatcher.jpg';
 
 class StartPage extends Component {
     render() {
         return (
             <div>
                 <Nav />
-
-                <div className="container full-height">
-                    <div className="content">
-                        Content
+                <div id="startpage">
+                    <div className="eyecather-wrapper">
+                        <img src={eyecather} id="eyecather" alt="eyecather" />
+                    </div>
+                    <div className="container">
+                        <div className="content">
+                            <h1>Aan de slag met Gentlestudent</h1>
+                            <h2>“Slagzin die aanzet tot actie en Gentlestudent voorstelt.”</h2>
+                            <form action="">
+                                <div className="search-wrapper">
+                                    <input type="text" placeholder="Probeer “Gent Korenmarkt”"/>
+                                    <button type="submit">Zoeken</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-
+                <Leerkansen />
                 <Footer />
             </div>
         );
