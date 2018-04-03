77bf0b8b5a5548cefd97e06f2e2428c31f560e13
Ismail Kutlu <ismail.kutlu@student.arteveldehs.be>
2018-04-03 19:11:30 +0200

    downloads fixed

--- app/src/Components/Frontpage/Download.js
+++ app/src/Components/Frontpage/Download.js
@@ -8,18 +8,22 @@ class Download extends Component {
     render() {
         return (
             <div id="download">
-                <div className="left">
-                    <img src={mockup} alt="mockup" />
-                </div>
-                <div className="right">
-                    <h1>Download de mobile app</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <a href="#">
-                        <img src={downloadiOS} alt="download-button-ios" />
-                    </a>
-                    <a href="#">
-                        <img src={downloadAndroid} alt="download-button-android" />
-                    </a>
+                <div className="container">
+                    <div className="content">
+                        <div className="left">
+                            <img src={mockup} alt="mockup" />
+                        </div>
+                        <div className="right">
+                            <h1>Download de mobile app</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <a href="#">
+                                <img src={downloadiOS} alt="download-button-ios" />
+                            </a>
+                            <a href="#">
+                                <img src={downloadAndroid} alt="download-button-android" />
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
