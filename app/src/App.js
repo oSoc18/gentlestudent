39b0ef41ae2525e58da4a13fbf1611d8b11e3a42
Ismail Kutlu <ismail.kutlu@student.arteveldehs.be>
2018-04-04 12:37:29 +0200

    react router added

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import FrontPage from './Screens/FrontPage';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import './App.css';
+// import Routes from './routes/router';
+import FrontPage from './Screens/FrontPage';
+import Leerkansen from './Screens/Leerkansen';
+import WordIssuer from './Screens/WordIssuer';
+import Ervaringen from './Screens/Ervaringen';
+import Nieuws from './Screens/Nieuws';
+import OverOns from './Screens/OverOns';
+
 
 class App extends Component {
 	render() {
 		return (
-			<FrontPage />
+			<Router>
+				<Switch>
+					<Route path="/" exact render={() => <FrontPage/>} />
+					<Route path="/leerkansen" exact render={() => <Leerkansen/>} />
+					<Route path="/wordissuer" exact render={() => <WordIssuer/>} />
+					<Route path="/ervaringen" exact render={() => <Ervaringen/>} />
+					<Route path="/nieuws" exact render={() => <Nieuws/>} />
+					<Route path="/overons" exact render={() => <OverOns/>} />
+				</Switch>
+			</Router>
 		);
 	}
 }
