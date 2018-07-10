2bce8f7fa99c88c75d3ab865e434f0a451047d77
Van Driessche Maxime <van-driessche-maxime@hotmail.com>
2018-07-10 16:03:06 +0200

    Attempt to provide login to Badgr api using sqflite db to store the login credentials

--- gentle_student/lib/data/rest_data.dart
+++ gentle_student/lib/data/rest_data.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 
+import 'package:Gentle_Student/utils/network_util.dart';
+import 'package:Gentle_Student/models/user.dart';
 
 //This class is a Singleton that handles REST API
 class RestData {
@@ -9,4 +12,23 @@ class RestData {
 
   //The URL used in every CRUD operation
   static const String BASE_URL = "";
+}
+
+class RestDatasource {
+  NetworkUtil _netUtil = new NetworkUtil();
+  static final BASE_URL = "https://api.badgr.io/";
+  static final LOGIN_URL = BASE_URL + "api-auth/token";
+  static final _API_KEY = "somerandomkey";
+
+  Future<User> login(String username, String password) {
+    return _netUtil.post(LOGIN_URL, body: {
+      "token": _API_KEY,
+      "username": username,
+      "password": password
+    }).then((dynamic res) {
+      print(res.toString());
+      if(res["error"]) throw new Exception(res["error_msg"]);
+      return new User.map(res["user"]);
+    });
+  }
 }
--- gentle_student/lib/pages/user/backpack/backpack_page.dart
+++ gentle_student/lib/pages/user/backpack/backpack_page.dart
@@ -1,3 +1,5 @@
+import 'package:Gentle_Student/data/rest_data.dart';
+import 'package:Gentle_Student/models/user.dart';
 import 'package:flutter/material.dart';
 
 class BackPackPage extends StatefulWidget {
@@ -7,7 +9,13 @@ class BackPackPage extends StatefulWidget {
 }
 
 class _BackPackPageState extends State<BackPackPage> {
-
+  doLogin(String username, String password) {
+    RestDatasource api = new RestDatasource();
+    api.login(username, password).then((User user) {
+      print("login succesfull");
+    }).catchError((Exception error) => print("login failed"));
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +24,13 @@ class _BackPackPageState extends State<BackPackPage> {
         iconTheme: new IconThemeData(color: Colors.white),
       ),
       backgroundColor: Colors.white,
+      body:
+        IconButton(
+            icon: Icon(Icons.account_circle, size: 48.0),
+            onPressed: () {
+              doLogin("van-driessche-maxime@hotmail.com", "yeix6chi");
+            },
+          )
     );
   }
 }
