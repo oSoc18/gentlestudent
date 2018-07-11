858fb49b35922d4a6b5f5b8b13beb9ca62791d84
BramDC3 <bramdeconinck3@gmail.com>
2018-07-11 14:14:44 +0200

    Firebase integration in Flutter works

--- gentle_student/lib/data/api.dart
+++ gentle_student/lib/data/api.dart
@@ -5,38 +5,9 @@ import 'package:Gentle_Student/models/difficulty.dart';
 import 'package:Gentle_Student/models/opportunity.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class OpportunityApi {
-  static FirebaseAuth _auth = FirebaseAuth.instance;
-  static GoogleSignIn _googleSignIn = new GoogleSignIn();
-
-  FirebaseUser firebaseUser;
-
-  OpportunityApi();
-
-  //OpportunityApi(FirebaseUser user) {
-    //this.firebaseUser = user;
-  //}
-
-  static Future<OpportunityApi> signInWithGoogle() async {
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    final FirebaseUser user = await _auth.signInWithGoogle(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    assert(user.email != null);
-    assert(user.displayName != null);
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
-
-    return new OpportunityApi();
-    //return new OpportunityApi(user);
-  }
+FirebaseUser firebaseUser;
 
   Future<List<Opportunity>> getAllOpportunities() async {
     return (await Firestore.instance.collection('Opportunities').getDocuments())
