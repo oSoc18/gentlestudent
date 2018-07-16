3dab3c915c653b3fbaabeb33d9f1c8a2043ffac0
Bram De Coninck <Bram@bramdeconinck.com>
2018-07-16 13:39:24 +0200

    Added the layout, needs api to work

--- gentle_student/lib/pages/user/my_learning_opportunities/my_learning_opportunities_page.dart
+++ gentle_student/lib/pages/user/my_learning_opportunities/my_learning_opportunities_page.dart
@@ -3,19 +3,76 @@ import 'package:flutter/material.dart';
 class MyLearningOpportunitiesPage extends StatefulWidget {
   static String tag = 'my-learning-opportunities-page';
   @override
-  _MyLearningOpportunitiesPageState createState() => _MyLearningOpportunitiesPageState();
+  _MyLearningOpportunitiesPageState createState() =>
+      _MyLearningOpportunitiesPageState();
 }
 
-class _MyLearningOpportunitiesPageState extends State<MyLearningOpportunitiesPage> {
+class _MyLearningOpportunitiesPageState
+    extends State<MyLearningOpportunitiesPage> {
+  //@override
+  //Widget build(BuildContext context) {
+  //return Scaffold(
+  //appBar: new AppBar(
+  //title: new Text("Mijn leerkansen", style: TextStyle(color: Colors.white)),
+  //iconTheme: new IconThemeData(color: Colors.white),
+  //),
+  //backgroundColor: Colors.white,
+  //);
+  //}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Mijn leerkansen", style: TextStyle(color: Colors.white)),
-        iconTheme: new IconThemeData(color: Colors.white),
+    return new DefaultTabController(
+      length: 2,
+      child: new Scaffold(
+        body: new NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              new SliverAppBar(
+                title: const Text(
+                  'Mijn leerkansen',
+                  style: TextStyle(color: Colors.white),
+                ),
+                iconTheme: new IconThemeData(
+                  color: Colors.white,
+                ),
+                forceElevated: innerBoxIsScrolled,
+                pinned: true,
+                floating: true,
+                bottom: new TabBar(
+                  labelColor: Colors.white,
+                  tabs: <Tab>[
+                    new Tab(text: 'Goedgekeurde'),
+                    new Tab(text: 'Aangevraagde'),
+                  ],
+                ),
+              ),
+            ];
+          },
+          body: new TabBarView(
+            children: <Widget>[
+              new Center(
+                child: new Container(
+                  height: 1000.0,
+                  color: Colors.green.shade200,
+                  child: new Center(
+                    child: new FlutterLogo(colors: Colors.green),
+                  ),
+                ),
+              ),
+              new Center(
+                child: new Container(
+                  height: 1000.0,
+                  color: Colors.purple.shade200,
+                  child: new Center(
+                    child: new FlutterLogo(colors: Colors.purple),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      backgroundColor: Colors.white,
     );
   }
 }
