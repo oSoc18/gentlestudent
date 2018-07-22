a35b0293adaba27e79590914be57060310b763d8
mrvdries <van-driessche-maxime@hotmail.com>
2018-07-22 21:50:17 +0200

    detail page progress

--- app/src/Components/Leerkansen/Detail.jsx
+++ app/src/Components/Leerkansen/Detail.jsx
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Spinner from '../Spinner';
+
 class Detail extends Component {
   render() {
     const { opportunities } = this.props;
-    let id = this.props.match.params.id;
+    const id = this.props.match.params.id;
 
     return (
-      <div>
-        <a href="/leerkansen">Back</a>
-        {/* <h1>{opportunities[id].title}</h1>
-        <p>{opportunities[id].description}</p> */}
-      </div>
+      <React.Fragment>
+				{ !! opportunities && <LeerkansDetail opportunity={ id } /> }
+				{ ! opportunities && <EmptyList/> }
+			</React.Fragment>
+      
     )
   }
 }
+
+const LeerkansDetail = ({ opportunity }) =>
+  <div>
+    <a href="/leerkansen">Back</a>
+    {/* {opportunity} */}
+    {/* {opportunity.shortDescription} */}
+    {/* <h1>{opportunities[id].title}</h1>
+    <p>{opportunities[id].description}</p> */}
+  </div>
+
+const EmptyList = () =>
+	<div>
+		<Spinner />
+	</div>
+
 
 export default Detail;
