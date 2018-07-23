178008434b140bf4831f864bed0edc6c787d01d3
mrvdries <van-driessche-maxime@hotmail.com>
2018-07-23 14:15:13 +0200

    small fix

--- app/src/Components/Firebase/firestore.js
+++ app/src/Components/Firebase/firestore.js
@@ -3,3 +3,7 @@ import { firestore } from './firebase';
 export const onceGetLeerkansen = () =>
   // db.ref('Opportunities').once('value');
   firestore.collection('Opportunities').get()
+
+  export const onceGetBadges = () =>
+  // db.ref('Opportunities').once('value');
+  firestore.collection('Badges').get()
