7368dd1297ac68595de2f1b7a2ebfcf788ab2288
mrvdries <van-driessche-maxime@hotmail.com>
2018-07-23 14:02:08 +0200

    fixing camelcase problem part 1

--- app/src/Components/Firebase/firestore.js
+++
@@ -1,8 +0,0 @@
-import { firestore } from './firebase';
-
-export const onceGetLeerkansen = () =>
-  // db.ref('Opportunities').once('value');
-  firestore.collection('Opportunities').get()
-
-export const onceGetBadges = () => 
-  firestore.collection('Badges').get()
