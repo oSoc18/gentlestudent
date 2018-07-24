bcbabaa25209c540d9d2e52fe4e9c0e5585cad47
Bram De Coninck <Bram@bramdeconinck.com>
2018-07-24 14:44:11 +0200

    A user can select a picture using the camera and the gallery

--- gentle_student/lib/pages/user/settings/settings_page.dart
+++ gentle_student/lib/pages/user/settings/settings_page.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:Gentle_Student/data/database_helper.dart';
 import 'package:Gentle_Student/pages/login/login_page.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SettingsPage extends StatefulWidget {
   static String tag = 'settings-page';
@@ -17,6 +19,7 @@ class _SettingsPageState extends State<SettingsPage> {
   final db = new DatabaseHelper();
   final scaffoldKey = new GlobalKey<ScaffoldState>();
   bool _switchValue = false;
+  File _image;
 
   //Function for switching between dark mode and light mode
   void _changeBrightnessAndColor() {
@@ -94,6 +97,50 @@ class _SettingsPageState extends State<SettingsPage> {
     );
   }
 
+  //Let user choose between camera and gallery
+  Future<Null> _displayCameraOrGalleryDialog() async {
+    return showDialog<Null>(
+      context: context,
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text("Profielfoto"),
+          content: new SingleChildScrollView(
+            child: new ListBody(
+              children: <Widget>[
+                new Text('Wilt u de camera gebruiken om een nieuwe foto nemen of wilt u een bestaande foto uit uw gallerij gebruiken?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('Camera'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                getImage(ImageSource.camera);
+              },
+            ),
+            new FlatButton(
+              child: new Text('Gallerij'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                getImage(ImageSource.gallery);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  //Let user take a picture or choose an existing picture from their gallery
+  Future getImage(ImageSource imageSource) async {
+    var image = await ImagePicker.pickImage(source: imageSource);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   //Location permission dialog
   Future<Null> _displayLocationPermissionDialog() async {
     return showDialog<Null>(
@@ -143,7 +190,7 @@ class _SettingsPageState extends State<SettingsPage> {
             child: ListTile(
               trailing: Icon(Icons.arrow_forward_ios),
               title: Text('Wijzig profielfoto'),
-              onTap: () {},
+              onTap: () => _displayCameraOrGalleryDialog(),
             ),
             decoration: new BoxDecoration(
               border: new Border(
