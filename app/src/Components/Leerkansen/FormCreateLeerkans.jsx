5c253381cb9cdbc1a62307a7e5e169e116630a78
mrvdries <van-driessche-maxime@hotmail.com>
2018-07-24 10:37:24 +0200

    pinImageUrl added to opportunity

--- app/src/Components/Leerkansen/FormCreateLeerkans.jsx
+++ app/src/Components/Leerkansen/FormCreateLeerkans.jsx
@@ -57,6 +57,25 @@ class FormCreateLeerkans extends React.Component {
     this.postNewAddress = this.postNewAddress.bind(this);
     this.postNewOpportunity = this.postNewOpportunity.bind(this);
     this.uploadImage = this.uploadImage.bind(this);
+    this.choosePin = this.choosePin.bind(this);
+  }
+
+  choosePin(){
+    let baseUrl = "https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Pins%2F";
+    let url = baseUrl;
+    switch(this.state.category){
+      case 0: url += "pin_digitale-geletterdheid";
+      case 1: url += "pin_duurzaamheid";
+      case 2: url += "pin_ondernemingszin";
+      case 3: url += "pin_onderzoekende-houding";
+      case 4: url += "pin_wereldburgerschap";
+    }
+    switch(this.state.difficulty){
+      case 0: url += "_1.png?alt=media";
+      case 1: url += "_2.png?alt=media";
+      case 2: url += "_3.png?alt=media";
+    }
+    this.setState({pinImageUrl: url});
   }
 
   handleChange(event) {
@@ -79,13 +98,14 @@ class FormCreateLeerkans extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    this.choosePin();
     if(this.state.image != ""){
       let fileName = this.state.title+"."+this.state.imageExtension;
       let baseUrl = "https://firebasestorage.googleapis.com/v0/b/gentle-student.appspot.com/o/Opportunity%20Images%2F";
       this.setState({imageUrl: baseUrl + encodeURIComponent(fileName)+"?alt=media"});
       this.uploadImage(fileName);
     }
-    // this.postNewAddress(this.postNewOpportunity);
+    this.postNewAddress(this.postNewOpportunity);
   }
 
   uploadImage(fileName){
