c4073efce3a390d92002905d38a4790f1470cdbb
Bram De Coninck <Bram@bramdeconinck.com>
2018-07-25 09:35:06 +0200

    Small irrelevant change

--- gentle_student/lib/navigation/home_page.dart
+++ gentle_student/lib/navigation/home_page.dart
@@ -151,7 +151,7 @@ class _HomePageState extends State<HomePage> {
     Beacons
         .ranging(
       region: new BeaconRegionIBeacon(
-        identifier: 'hallo',
+        identifier: 'Gentlestudent beacons',
         proximityUUID: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D',
       ),
       inBackground: true,
